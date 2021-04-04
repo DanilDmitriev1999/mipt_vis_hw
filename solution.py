291402b1f70379bea240c0b7844fe76dd465f87a
DanilDmitriev1999 <danildmitrievv11@gmail.com>
2021-04-04 21:15:24 +0300

    изменил немного алгоритм

--- solution.py
+++ solution.py
@@ -3,59 +3,67 @@ import networkx as nx
 
 import matplotlib.pyplot as plt
 
-class LayeredTree():
+
+class LayeredTree:
     def __init__(self, filename):
         self.filename = filename
         self.coordinates = {}
         self.tree = nx.read_graphml(self.filename)
+        self.max_level = len(list(nx.dag_longest_path(self.tree)))
 
-    def calculate_coordinates(self, tree, node, previous_node):
+    def calculate_x_coordinates(self, node, previous_node):
         offset = 0
         maxx = 0
         sum = 0
         count = 0
-        child_y = 0
 
         if len(list(nx.neighbors(self.tree, node))) == 0:
             self.tree.nodes[node]['corr_x'] = 0
-            self.tree.nodes[node]['corr_y'] = child_y
             return
 
         for current_node in list(nx.neighbors(self.tree, node)):
             if current_node != previous_node:
-                self.calculate_coordinates(self.tree, current_node, node)
+                self.calculate_x_coordinates(current_node, node)
 
         for current_node in nx.neighbors(self.tree, node):
             if current_node != previous_node:
-                maxx = self.move_tree(self.tree, current_node, node, offset, maxx)
+                maxx = self.move_tree(current_node, node, offset, maxx)
                 offset = maxx + offset + 2
-                child_y = max(self.tree.nodes[current_node]['corr_y'], child_y)
                 count += 1
                 sum += self.tree.nodes[current_node]['corr_x']
-        tree.nodes[node]['corr_x'] = int(sum / count)
-        tree.nodes[node]['corr_y'] = child_y + 1
-        x = 0
+        self.tree.nodes[node]['corr_x'] = int(sum / count)
+        # x = 0
 
-    def move_tree(self, tree, node, previous_node, offset, maxx):
+    def calculate_y_coordinates(self, node, previous_node, level):
+        if len(list(nx.neighbors(self.tree, node))) == 0:
+            self.tree.nodes[node]['corr_y'] = level
+            return
+        for current_node in list(nx.neighbors(self.tree, node)):
+            if current_node != previous_node:
+                self.calculate_y_coordinates(current_node, node, level - 1)
+
+        self.tree.nodes[node]['corr_y'] = level
+
+    def move_tree(self, node, previous_node, offset, maxx):
         self.tree.nodes[node]['corr_x'] += offset
-        maxx = max(tree.nodes[node]['corr_x'], maxx)
-        if len(list(nx.neighbors(tree, node))) == 0:
+        maxx = max(self.tree.nodes[node]['corr_x'], maxx)
+        if len(list(nx.neighbors(self.tree, node))) == 0:
             return maxx
         for current_node in list(nx.neighbors(self.tree, node)):
             if current_node != previous_node:
-                maxx = max(maxx, self.move_tree(self.tree, current_node, node, offset, maxx))
+                maxx = max(maxx, self.move_tree(current_node, node, offset, maxx))
         return maxx
 
     def draw(self):
-        self.calculate_coordinates(self.tree, 'n0', 'n0')
+        self.calculate_x_coordinates('n0', 'n0')
+        self.calculate_y_coordinates('n0', 'n0', self.max_level)
 
-        coordinates = {}
         for node in list(self.tree.nodes):
-            coordinates[node] = (self.tree.nodes[node]['corr_x'], self.tree.nodes[node]['corr_y'])
+            self.coordinates[node] = (self.tree.nodes[node]['corr_x'], self.tree.nodes[node]['corr_y'])
 
         plt.figure(figsize=(20, 10))
 
-        nx.draw(self.tree, pos=coordinates, with_labels=True)
+        nx.draw(self.tree, pos=self.coordinates, with_labels=True)
         plt.show()
 
 
